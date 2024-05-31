07c0dc75ba6a720b35ce5979b4530ea4cc88f4ad
mikit1234 <mikaliyew0@gmail.com>
2024-05-31 10:59:45 -0700

    search.php

--- search.php
+++ search.php
@@ -24,3 +24,64 @@ if (isset($_SESSION['username'])) {
          foreach ($users as $user) {
          	if ($user['user_id'] == $_SESSION['user_id']) continue;
        ?>
+       <li class="list-group-item">
+		<a href="chat.php?user=<?=$user['username']?>"
+		   class="d-flex
+		          justify-content-between
+		          align-items-center p-2">
+			<div class="d-flex
+			            align-items-center">
+
+			    <img src="uploads/<?=$user['p_p']?>"
+			         class="w-10 rounded-circle">
+
+			    <h3 class="fs-xs m-2">
+			    	<?=$user['name']?>
+			    </h3>            	
+			</div>
+		 </a>
+	   </li>
+       <?php } }else { ?>
+         <div class="alert alert-info 
+    				 text-center">
+		   <i class="fa fa-user-times d-block fs-big"></i>
+           The user "<?=htmlspecialchars($_POST['key'])?>"
+           is  not found.
+		</div>
+    <?php }
+    }
+
+}else {
+	header("Location: ../../index.php");
+	exit;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
