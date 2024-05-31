76613563bc4b9254a2ba9eef97521b9e739ecd64
mikit1234 <mikaliyew0@gmail.com>
2024-05-31 11:35:37 -0700

    search.php

---
+++ search.php
@@ -0,0 +1,26 @@
+<?php
+
+session_start();
+
+# check if the user is logged in
+if (isset($_SESSION['username'])) {
+    # check if the key is submitted
+    if(isset($_POST['key'])){
+       # database connection file
+	   include '../db.conn.php';
+
+	   # creating simple search algorithm :) 
+	   $key = "%{$_POST['key']}%";
+     
+	   $sql = "SELECT * FROM users
+	           WHERE username
+	           LIKE ? OR name LIKE ?";
+       $stmt = $conn->prepare($sql);
+       $stmt->execute([$key, $key]);
+
+       if($stmt->rowCount() > 0){ 
+         $users = $stmt->fetchAll();
+
+         foreach ($users as $user) {
+         	if ($user['user_id'] == $_SESSION['user_id']) continue;
+       ?>
